0fb663044c1e881ff9539b4af57a3af8605e52cc
Tekum Emmaanuella <tekumnjit@gmail.com>
2025-11-17 16:18:44 +0100

    feat: Add full CI pipelines for frontend and backend

--- likelee-ui/eslint.config.js
+++ likelee-ui/eslint.config.js
@@ -1,10 +1,19 @@
+import globals from "globals";
 import js from '@eslint/js'
-import globals from 'globals'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 
 export default [
+  {
+    // This rule is ONLY for these config files
+    files: ['tailwind.config.js', 'vite.config.js', 'postcss.config.js', 'eslint.config.js'],
+    languageOptions: {
+        globals: {
+            ...globals.node, // This tells the linter to allow all Node.js keywords
+        }
+    }
+  },
   { ignores: ['dist'] },
   {
     files: ['**/*.{js,jsx}'],
