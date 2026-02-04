57d708419749679fc65e28280897499734fae0c3
lele-maxwell <leleivanlele22@gmail.com>
2026-02-04 15:35:29 +0100

    fix: data base tables consolidate

--- supabase/migrations/0021_add_password_protection.sql
+++
@@ -1,8 +0,0 @@
--- Adds password_protected flag to talent packages
-
-BEGIN;
-
-ALTER TABLE public.agency_talent_packages
-ADD COLUMN IF NOT EXISTS password_protected BOOLEAN DEFAULT FALSE;
-
-COMMIT;
