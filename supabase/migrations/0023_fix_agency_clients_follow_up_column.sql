521acfa3bc52617bcd11b7e694c93821e85ef4dc
lele-maxwell <leleivanlele22@gmail.com>
2026-02-10 14:05:13 +0100

    feat: Incorporate `creator_id` nullability and `next_follow_up_date` column into base schema migrations, deleting their respective fix migrations.

--- supabase/migrations/0023_fix_agency_clients_follow_up_column.sql
+++
@@ -1,9 +0,0 @@
--- 0023_fix_agency_clients_follow_up_column.sql
-BEGIN;
-
--- Add missing next_follow_up_date column to agency_clients
--- This column was used in the code but accidentally omitted from previous migrations
-ALTER TABLE public.agency_clients
-  ADD COLUMN IF NOT EXISTS next_follow_up_date date;
-
-COMMIT;
